d7ac246b4a680856dce3c9f23597200dc02a9365
Tiro Modibedi <tiromodibedi39@gmail.com>
2018-07-13 12:17:33 +0200

    Made changes to the side-navbar

--- src/app/components/side-navbar/side-navbar.component.ts
+++ src/app/components/side-navbar/side-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-side-navbar',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-navbar.component.css']
 })
 export class SideNavbarComponent implements OnInit {
+  loggedInUser: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe((auth) => {
+      if (auth) {
+        this.loggedInUser = auth.email;
+      }
+    });
   }
-
 }
