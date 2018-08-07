ec40a78d1cb4fde52291352ee93eb55056e0cf6d
Ji Brust <jiaying1@me.com>
2018-08-07 12:13:52 +0200

    added hover and fixed small issues

--- src/app/components/secondary/components/dashboard/dashboard.component.ts
+++ src/app/components/secondary/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BursaryService } from '../../../../services/bursary.service';
+import { Bursary } from '../../../../models/Bursary';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  bursaries: Bursary[]= [];
 
-  constructor() { }
+  bursaryinfo: boolean;
+
+  selectedBursary: Bursary;
+
+  constructor(private bursaryService: BursaryService) { }
 
   ngOnInit() {
+    this.bursaryService.getBursaries().subscribe(Data => {
+      this.bursaries = Data;
+    });
+    this.bursaryinfo= false;
+  }
+
+  showInfo(bursary: Bursary) {
+    this.bursaryinfo= true;
+    this.selectedBursary= bursary;
   }
 
 }
